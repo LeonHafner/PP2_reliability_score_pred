98ad8795911091d6efcd0a2c02932225e4db727f
Tentenn <kok.tang@gmx.de>
2024-11-24 12:32:35 +0100

    add apc experiment and adata

--- src/process_batch.py
+++ src/process_batch.py
@@ -1,7 +1,8 @@
 # Usage
 # pip install tqdm scipy numpy jax h5py
-# python process_batch train_X.csv output_folder/
+# python process_batch train_X.csv output_folder/ <apc>
 # train_X.csvs are on Google Drive
+
 
 import time
 import os 
@@ -14,6 +15,7 @@ from utils import *
 import random 
 import matplotlib.pyplot as plt
 import sys
+
 
 def get_categorical_jacobian(seq, model, alphabet, device):
   # ∂in/∂out
@@ -29,31 +31,57 @@ def get_categorical_jacobian(seq, model, alphabet, device):
       fx_h[n] = f(x_h)
     return fx_h - fx
 
-def get_contacts(x, symm=True, center=True, rm=1):
+def get_contacts(x, symm=True, center=True, rm=1, apc=False):
   # convert jacobian (L,A,L,A) to contact map (L,L)
   j = x.copy()
   if center:
     for i in range(4): j -= j.mean(i,keepdims=True)
   j_fn = np.sqrt(np.square(j).sum((1,3)))
   np.fill_diagonal(j_fn,0)
-  j_fn_corrected = do_apc(j_fn, rm=rm)
-  if symm:
-    j_fn_corrected = (j_fn_corrected + j_fn_corrected.T)/2
-  return j_fn_corrected
+    
+  if apc:
+      j_fn_corrected = do_apc(j_fn, rm=rm)
+      if symm:
+        j_fn_corrected = (j_fn_corrected + j_fn_corrected.T)/2
+      return j_fn_corrected
+  else:
+      return j_fn
+
+def do_apc(x, rm=1):
+  '''given matrix do apc correction'''
+  # trying to remove different number of components
+  # rm=0 remove none
+  # rm=1 apc
+  x = np.copy(x)
+  if rm == 0:
+    return x
+  elif rm == 1:
+    a1 = x.sum(0,keepdims=True)
+    a2 = x.sum(1,keepdims=True)
+    y = x - (a1*a2)/x.sum()
+  else:
+    # decompose matrix, rm largest(s) eigenvectors
+    u,s,v = np.linalg.svd(x)
+    y = s[rm:] * u[:,rm:] @ v[rm:,:]
+  np.fill_diagonal(y,0)
+  return y
 
 def main():
     # Usage
     # pip install tqdm scipy numpy jax h5py
-    # python process_batch train_X.csv output_folder/
+    # python process_batch train_X.csv output_folder/ <apc>
+    # apc argument is optional
     
     filepath_to_protein_csv = sys.argv[1]
     out_folder = sys.argv[2]
+    apc_on = True if sys.argv[3] == "apc" else False
     print("Start Processing: ", filepath_to_protein_csv)
     print("Outfolder: ", out_folder)
+    print(f"APC?: {apc_on}")
+    
+    
     print("\n\nLoading model, ... could take a while \n\n ")
-    # LOAD MODEL AND PUT TO GPU / Validation: higher similiarity ok: 50%
     model, alphabet = torch.hub.load("facebookresearch/esm:main", "esm2_t33_650M_UR50D")
-    # put model on GPU if available
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     os.environ["CUDA_VISIBLE_DEVICES"] = '2'
     model = model.to(device)
@@ -68,7 +96,7 @@ def main():
             if ";" in line:
                 pid, sequence = line.split(";")
                 cjm1 = get_categorical_jacobian(sequence, model, alphabet, device)
-                contact_map = get_contacts(cjm1)
+                contact_map = get_contacts(cjm1, model, alphabet, apc=apc_on)
                 out_path = os.path.join(out_folder, pid+".npy")
                 np.save(out_path, contact_map)
 
